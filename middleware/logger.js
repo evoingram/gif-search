523f8cded28134a515379693b40794a73da27541
Erica L. Ingram <inquiries@aquoco.co>
2022-01-16 09:55:10 -0800

    feat: [] fixing linter issues

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,6 +1,6 @@
-logger = (req, res, next) => {
+function logger(req, res, next) {
 	console.log(`${req.method} to ${req.url} at ${new Date().toISOString()}`)
 	next();
-};
+}
 
 module.exports = logger;
