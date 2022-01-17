61f8fc0efccfa8229936bfa81285ec68dbab8016
Erica L. Ingram <inquiries@aquoco.co>
2022-01-16 17:10:49 -0800

    feat: [#11] added update users router

--- users/user-router.js
+++ users/user-router.js
@@ -7,6 +7,7 @@ const Users = require('./user-model.js');
 
 // const restricted = require('../auth/restriction.js');
 
+// GET:  get list of all users
 router.get('/', (req, res) => {
 	Users.find()
 		.then(users => {
@@ -15,6 +16,7 @@ router.get('/', (req, res) => {
 		.catch(err => res.send(err));
 });
 
+// GET:  get individual user
 router.get('/:id', (req, res) => {
 	const id = req.params.id;
 	if (!id) {
@@ -30,6 +32,25 @@ router.get('/:id', (req, res) => {
 	}
 });
 
+// PUT:  update user
+router.put('/:id', (req, res) => {
+	const usersid = req.params.usersid;
+	const updatedUser = req.body;
+
+	Users.update(usersid, updatedUser)
+		.then((user) => {
+			if (user) {
+				res.json(user);
+			} else {
+				res.status(404).json({ message: `Could not find user with given id ${usersid}.` });
+			}
+		})
+		.catch((err) => {
+			res.status(500).json({ message: 'Failed to update user.', error: err });
+		});
+});
+
+// DELETE:  delete a user
 router.delete('/:id', (req, res) => {
 	const id = req.params.id;
 	if (!id) {
